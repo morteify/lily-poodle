1119b29773f06882b05ee3e0c18713bdcb027f10
morteify <weglarz.michal@outlook.com>
2021-01-31 04:53:51 +0100

    chore: apply lint changes

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
   },
 };
